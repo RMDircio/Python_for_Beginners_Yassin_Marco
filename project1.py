15537486c866319310e8b7ca7e6ba06918103bcb
RMDircio <reginadircio@gmail.com>
2020-12-15 22:37:46 -0600

    String Manipulation

--- project1.py
+++ project1.py
@@ -4,6 +4,8 @@ print('Hello World')
 ##  playing with string and variables  ##
 # ------------------------------------- #
 
+print('-------------------------------')
+
 # variables
 person_name = 'Mr. Jack'
 
@@ -24,7 +26,48 @@ print('Thank you, ' + person_name )
 ##     playing with Boolean values     ##
 # ------------------------------------- #
 
+print('-------------------------------')
+
 having_a_house = True
 
 if having_a_house:
     print('I own a small house')
+
+print('-------------------------------')
+
+# ------------------------------------- #
+##         String Manipulation         ##
+# ------------------------------------- #
+
+sentence = 'It is Taco Time yet?'
+
+# make every character uppercase
+print('This is all uppercase:', sentence.upper())
+
+# make every character lowercase
+print('This is all lowercase:', sentence.lower())
+
+# verify that all characters are lowercase
+print('Are all the characters in "sentence", lowercase?:', sentence.islower())
+
+# chain the commands to make a true case for lowercase
+print('With chaining, are all the characters in "sentence", lowercase?:', sentence.lower().islower())
+
+# what is the length of 'sentence'?
+print('How many characters are in "sentence"?:', len(sentence))
+
+# replace a word in "sentence"
+# replace('word_to_take_out', 'word_to_step_in)
+print('Using replace here:', sentence.replace('yet?', 'Now!')) # string must be EXACT
+
+# get the index of a character
+# only gets the first instance
+print('What is the index for character "t":', sentence.index('t'))
+
+# get the index of a word
+print('What is the index for "yet?"?:', sentence.index('yet?'))
+
+# split up the text
+print('Using the "\ n" here \n makes a line break' )
+
+print('-------------------------------')
