a9691d96d231038745e271c3d88253257aa863a5
RMDircio <reginadircio@gmail.com>
2020-12-19 19:58:58 -0600

    Utility of Functions

--- Part2.py
+++ Part2.py
@@ -2,10 +2,37 @@
 ##        Utility of Functions         ##
 # ------------------------------------- #
 
+# # what do you love function
+# def love(name):
+#     print(f'I love {name}')
+
+# love('Muffin')
+
+# # write a monologe
+# def person_1(name):
+#     print(f'{name}: Hello, how can help you?')
 
 
+# def person_2(food, drink, dessert, name):
+#     name = input('What is your name? ')
+#     food = input('What would you like to eat? ')
+#     drink = input('What would like to drink? ')
+#     dessert = input('What would like for dessert? ')
+#     print(f'{name}: I would like {food}, and {drink} to drink and for dessert, {dessert}, please')
+
+# def person_1_1(name):
+#     print(f'{name}: Yes, of course, thank you.')
 
 
+# person_1('Cashier')
+# person_2('food', 'drink', 'dessert', 'name')
+# person_1_1
+# print('-------------------------------')
+
+
+# ------------------------------------- #
+##      Use the Return Statement       ##
+# ------------------------------------- #
 
 
 
